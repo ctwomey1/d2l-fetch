16fe7aef17592972820fd9d215f0764b1e51715e
Ryan Mahler <Ryan.Mahler@D2L.com>
2017-10-02 17:33:32 -0400

    Make bower-able
    Adding this file allows us to use bower to control the versioning of this package, rather than using the CDNified versions and hoping that they won't conflict. This will also require similar updates in the d2l-fetch-* middlewares. This file is automatically updated by Travis to point at the CDN-ified script that will be generated by frau-ci in the next step. Since the new version isn't released until _after_ this commit is pushed, it does theoretically mean that if somebody pulled master at just the right moment, d2l-fetch.html would be pointing to a non-existant file, but oh well. (If the consumer is using a proper bower version, this situation can't occur since the new version is only generated after the new file is CDN-ified.)

---
+++ update.sh
@@ -0,0 +1,53 @@
+#!/bin/bash
+set -e
+
+if ! [ "$TRAVIS_BRANCH" == "master" ]; then
+	echo "d2l-fetch.html is only updated on master"
+	exit 0
+fi
+
+lastVersion=$(git describe --abbrev=0)
+echo "Last version was" $lastVersion
+
+versionRegex='^([0-9]+)\.([0-9]+)\.([0-9]+)$'
+if ! [[ $lastVersion =~ $versionRegex ]]; then
+	echo $lastVersion "is not a valid semver string according to this shitty regex"
+	exit 1
+fi
+
+majorVersion=${BASH_REMATCH[1]}
+minorVersion=${BASH_REMATCH[2]}
+patchVersion=${BASH_REMATCH[3]}
+
+# Uses syntax from https://github.com/Brightspace/frau-ci/blob/master/lib/travis-ci/update-version.js
+logMessage=$(git log -1 --pretty=format:%s%b)
+majorRegex='increment major'
+patchRegex='increment patch'
+commitSlug=""
+if [[ $logMessage =~ $majorRegex ]]; then
+	echo "Incrementing major version"
+	majorVersion=$((majorVersion + 1))
+	commitSlug="[increment major]"
+elif [[ $logMessage =~ $patchRegex ]]; then
+	echo "Incrementing patch version"
+	patchVersion=$((patchVersion + 1))
+	commitSlug="[increment patch]"
+else
+	echo "Incrementing minor version"
+	minorVersion=$((minorVersion + 1))
+	commitSlug="[increment minor]"
+fi
+
+newVersion="${majorVersion}.${minorVersion}.${patchVersion}"
+
+echo "New version is" $newVersion
+
+echo "<!-- CHANGES TO THIS FILE WILL BE LOST - IT IS AUTOMATICALLY GENERATED WHEN d2l-fetch IS RELEASED -->" > d2l-fetch.html
+echo "<script src=\"https://s.brightspace.com/lib/d2lfetch/"$newVersion"/d2lfetch.js\"></script>" >> d2l-fetch.html
+
+git remote add upstream "https://${GH_TOKEN}@github.com/Brightspace/d2l-fetch.git"
+git config --global user.email "travis@travis-ci.com"
+git config --global user.name "Travis CI"
+git add d2l-fetch.html
+git commit -m "${commitSlug} Update d2l-fetch.html to v${newVersion}"
+git push upstream HEAD:master
